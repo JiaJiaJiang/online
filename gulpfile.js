e1f05bf6d4e6b1f898f114eb4ce5d38489f0b949
LuoJia <luojia@luojia.me>
2021-07-25 15:32:39 +0800

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -5,36 +5,61 @@ var dist='./client';
 //js
 function transjs(name,cover=90){
 	var browserify = require('browserify'),
+		buffer = require('vinyl-buffer'),
 		source = require('vinyl-source-stream'),
-		rename = require('gulp-rename');
-
+		rename = require('gulp-rename'),
+		// babel = require('gulp-babel'),
+		sourcemaps = require('gulp-sourcemaps');
 	console.log(`compiling ${name} covers ${cover}% browsers`);
 
 	return browserify({
 			entries: name,
 			basedir:'./client/',
-			debug: true,
+			debug: false,
 			sourceType: 'module'
 		})
 		.transform(
 			"babelify",{
+				sourceMaps: true,
 				presets: [
+					["minify", {
+						mangle:false,
+					}],
 					[
 						"@babel/preset-env",{
 							"targets":{ 
 								"browsers":`cover ${cover}%`
 							},
 							"corejs":3,
+							"bugfixes":true,
 							"debug": true,
 							"useBuiltIns": 'usage'
 						}
 					],
 				],
+				plugins:[
+				"babel-plugin-remove-comments",
+				'@babel/plugin-proposal-class-properties',
+					"@babel/plugin-proposal-export-default-from",
+				]
 			}
 		)
 		.bundle()
-		.pipe(source(`./${name}`))
+		.pipe(source(name))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		/* .pipe(babel({
+			presets:[
+				["minify", {
+					mangle:false,
+				}],
+			],
+			plugins:[
+				"babel-plugin-remove-comments",
+			]
+		})) */
 		.pipe(rename({extname:`.${cover}.js`}))
+		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(dist));
 }
 gulp.task('js-main',function(){
